b99dee7c978072bafc6be70be7f8e61a79d22b9d
Maxim Ivashkov <33482502+maximnl@users.noreply.github.com>
2023-04-19 11:48:42 +0200

    Update 01_a_time_date_table.sql
    some new columns
    some chart types changed to numeric , for powerbi chart axis scaling purposes

--- 01_a_time_date_table.sql
+++ 01_a_time_date_table.sql
@@ -1,40 +1,47 @@
-
--- this is time date dimension (all dimensions that has to do with time are prefixed TIME. 
-SET ANSI_NULLS ON
+ SET ANSI_NULLS ON
 GO
-
 SET QUOTED_IDENTIFIER ON
 GO
-
-CREATE TABLE [A_TIME_DATE](
+CREATE TABLE [dbo].[A_TIME_DATE](
 	[date] [date] NOT NULL,
 	[date_key] [int] NULL,
 	[date_key_char] [char](8) NULL,
-	[year] [char](4) NULL,
-	[year52] [char](4) NULL,
-	[year_quarter_char] [char](7) NULL,
-	[year_month_char] [char](7) NULL,
-	[year_week] [char](6) NULL,
-	[year_week_long] [char](7) NULL,
+	[year] [smallint] NULL,
+	[year_char] [char](4) NULL,
+	[year52] [smallint] NULL,
+	[year52_char] [char](4) NULL,
+	[year_week] [int] NULL,
+	[year_week_char] [char](6) NULL,
 	[year_week_short] [char](5) NULL,
-	[semester] [char](10) NULL,
-	[quarter] [char](10) NULL,
-	[month] [char](2) NULL,
-	[month_quarter] [char](10) NULL,
-	[month_long] [char](10) NULL,
+	[year_week_long] [char](7) NULL,
+	[year_month] [int] NULL,
+	[year_month_char] [char](7) NULL,
+	[year_month_short] [char](5) NULL,
+	[year_month_long] [char](7) NULL,
+	[year_quarter] [int] NULL,
+	[year_quarter_char] [char](7) NULL,
+	[year_quarter_short] [char](4) NULL,
+	[year_quarter_long] [char](6) NULL,
+	[semester] [tinyint] NULL,
+	[quarter] [tinyint] NULL,
+	[month] [tinyint] NULL,
+	[month_char] [char](2) NULL,
+	[month_quarter] [tinyint] NULL,
 	[month_short] [char](3) NULL,
+	[month_long] [char](10) NULL,
 	[month_days] [smallint] NULL,
 	[months_2000] [int] NULL,
-	[week] [char](2) NULL,
-	[week_quarter] [char](2) NULL,
-	[week_month] [char](10) NULL,
+	[week] [tinyint] NULL,
+	[week_char] [char](2) NULL,
+	[week_quarter] [tinyint] NULL,
+	[week_month] [tinyint] NULL,
 	[weeks_2000] [int] NULL,
 	[day_long] [char](10) NULL,
 	[day_short] [char](2) NULL,
-	[day_year] [char](3) NULL,
-	[day_quarter] [char](10) NULL,
-	[day_month] [char](2) NULL,
-	[day_week] [char](1) NULL,
+	[day_year] [smallint] NULL,
+	[day_quarter] [smallint] NULL,
+	[day_month] [smallint] NULL,
+	[day_week] [smallint] NULL,
 	[days_2000] [int] NULL,
 	[YYYYMMDD] [char](8) NULL,
 	[MM/DD/YYYY] [char](10) NULL,
@@ -42,10 +49,12 @@ CREATE TABLE [A_TIME_DATE](
 	[YYYY-MM-DD] [char](10) NULL,
 	[DD-MM-YYYY] [char](10) NULL,
 	[MMM DD YYYY] [char](11) NULL,
-	[country] [char](2) NULL,
- CONSTRAINT [PK_A_TIME_DATE] PRIMARY KEY CLUSTERED 
+	[epoch] [bigint] NULL,
+	[country] [char](2) NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[A_TIME_DATE] ADD  CONSTRAINT [PK_A_TIME_DATE_] PRIMARY KEY CLUSTERED 
 (
 	[date] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 GO
